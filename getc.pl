ebc2fdf1b8f359567c0aea9a52ff23d86daf048e
Kyle Johnson <kjohnson@fixertec.net>
2009-08-12 15:11:36 -0400

    Added error handling to getc and a sleep(1) before getc

--- getc.pl
+++ getc.pl
@@ -12,8 +12,8 @@ select((select($PORT), $|=1)[0]); # Make $PORT hot
 
 print $PORT chr(254); # 254 tells the device to listen for commands
 print $PORT chr(27); # 27 enables reporting and should send back 85
-
-my $char = getc($PORT); # Get the respond (85)
+sleep(1);
+my $char = getc($PORT) || die "error! $!"; # Get the respond (85)
 print ord($char) . "\n"; # Print the response
 
 close($PORT);
