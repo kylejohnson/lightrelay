df52b7b3195a9a9956a1ca17f78eb3d4f4bb91d7
Kyle Johnson <kjohnson@fixertec.net>
2009-08-12 12:23:35 -0400

    Writing in my getc script works.  Added code for reading.

--- getc.pl
+++ getc.pl
@@ -4,12 +4,17 @@
 
 use strict;
 use warnings;
+use Fcntl;
 
 my $port = "/dev/ttyS0";
+my $PORT;
 
-sysopen(PORT, $port, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
+sysopen($PORT, $port, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
 
-print PORT chr(254);
-print PORT chr(27); # 27 enables reporting - should send back 85
+print $PORT chr(254);
+print $PORT chr(27); # 27 enables reporting - should send back 85
 
-close(PORT);
+my $char = getc($PORT);
+print ord($char);
+
+close($PORT);
