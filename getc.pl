cd1b91c28631b355508adce140f89169e3fc74f6
Kyle Johnson <kjohnson@fixertec.net>
2009-08-12 12:29:11 -0400

    Added code for buffering / flushing.

--- getc.pl
+++ getc.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
 
-# Trying out getc.
-
 use strict;
 use warnings;
 use Fcntl;
@@ -9,7 +7,8 @@ use Fcntl;
 my $port = "/dev/ttyS0";
 my $PORT;
 
-sysopen($PORT, $port, O_RDWR | O_NOCTTY | O_NDELAY | O_NONBLOCK);
+sysopen($PORT, $port, O_RDWR | O_NONBLOCK) || die "can not open $port: $!\n";
+my $ofh = select($PORT); $| = 1; select($ofh);
 
 print $PORT chr(254);
 print $PORT chr(27); # 27 enables reporting - should send back 85
