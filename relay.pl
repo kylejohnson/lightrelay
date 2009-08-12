7b3bb9801bcc7a7138b210dbe978cdf0246ef034
Kyle Johnson <kjohnson@fixertec.net>
2009-08-12 11:16:02 -0400

    Initial commit
     * Trying to write to and read from device using Device::SerialPort;
     * Writing with write();
     * Reading with read(255);

---
+++ relay.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Device::SerialPort;
+
+my $portName = "/dev/ttyS0";
+my $portObj = new Device::SerialPort($portName) || die "Can't open $portName: $!\n";
+my $cmd1 = chr(254);
+my $cmd2 = chr(150);
+
+$portObj->baudrate(115200);
+$portObj->parity("none");
+$portObj->databits(8);
+$portObj->stopbits(1);
+$portObj->handshake("none");
+
+$portObj->write_settings;
+
+$portObj->write($cmd1);
+$portObj->write($cmd2);
+$portObj->are_match();
+$portObj->lookclear;
+$portObj->read(255);
+
+
