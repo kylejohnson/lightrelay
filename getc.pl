e20b607173f344248e361a85a20e6973b94a91d8
Kyle Johnson <kjohnson@fixertec.net>
2009-08-18 11:23:20 -0400

    Code started working and I do not know why.  Code reflects what it was the last time that I had it working.
     * Simple print DEV chr() & getc DEV

--- getc.pl
+++ getc.pl
@@ -2,32 +2,13 @@
 
 use strict;
 use warnings;
-use Fcntl;
-use IO::Select;
 
-my $port = "/dev/ttyS0";
-my $PORT;
-my $read_set = new IO::Select();
-my $line;
+my $port = "/dev/ttyUSB0";
 
-sysopen($PORT, $port, O_RDWR | O_NONBLOCK) || die "can not open $port: $!\n";
-select((select($PORT), $|=1)[0]); # Make $PORT hot
+open (SERIALPORT, "+<", "$port") or die "can't open $port. ";
+print SERIALPORT chr(254);
+print SERIALPORT chr(27);
+my $result = getc(SERIALPORT);
+print ord($result) . "\n";
 
-print $PORT chr(254); # 254 tells the device to listen for commands
-print $PORT chr(27); # 27 enables reporting and should send back 85
-
-$read_set->add($PORT);
-while (1) {
- my ($rh_set) = IO::Select->select($read_set, undef, undef, 0);
- foreach my $rh (@$rh_set) {
-  if ($rh != $PORT) {
-    sysread($rh, $line, 8);
-    print $line;
-   }
-  }
- }
-
-#my $char = getc($PORT) || die "error! $!"; # Get the respond (85)
-#print ord($char) . "\n"; # Print the response
-
-close($PORT);
+close (SERIALPORT);
