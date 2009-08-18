f36ac3a1be4b381f053589e155ca9720cebcc796
Kyle Johnson <kjohnson@fixertec.net>
2009-08-18 15:25:30 -0400

    Modified code to only print first 2 channels, and to round to 2 decimal places

--- cycle.pl
+++ cycle.pl
@@ -8,27 +8,32 @@ my $count = 150;
 my $cmd = chr(254);
 my $cycle = 1;
 my $channel = 0;
+our $voltage;
 
 #system("/bin/stty 115200 ignbrk -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -F $port") == 0 || die "Can't stty: $!\n";
 open(my $DEV, "+<", $port) || die "Can't open $port: $!\n";
-while ($count <= 157) {
+while ($count <= 151) {
+ if ($count == 150) {
+  print "\t $cycle\n";
+ }
  print $DEV $cmd;
  print $DEV chr($count);
  my $result = ord(getc($DEV));
  if ($result == 255) {
-  my $voltage = 0;
-  print "Channel $channel:\t " . $voltage . " Volts\n";
+  $voltage = 0;
  } else {
-  my $voltage = ($result * 0.019607);
-  print "Channel $channel:\t " . $voltage . " Volts\n";
+
+  $voltage = sprintf("%.2f", ($result * 0.019607));
  }
+ printf "Channel $channel:\t " . $voltage . " Volts ($result)\n";
  $count++;
+ select(undef,undef,undef,.50);
  $channel++;
 
-if ($count > 157) {
+if ($count > 151) {
  $count = 150;
  $channel = 0;
- print "\t $cycle\n";
+ print "\n";
  $cycle++;
  sleep(1);
 }
