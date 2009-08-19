5d7b010f0eb2cca9ca5d82d7820eefb466abd85e
Kyle Johnson <kjohnson@fixertec.net>
2009-08-19 16:47:56 -0400

    Added subroutine to calculate speed in mph

--- speed.pl
+++ speed.pl
@@ -8,6 +8,7 @@ our $distance = 12; # Distance between pucks
 our $polltime = .03125; # Time between each poll
 our $cmd = chr(254);
 our ($chan1, $chan2, $time1, $time2);
+our $timeout = 4;
 
 open(my $DEV, "+<", $port) || die "can't open $port: $!";
 
@@ -17,11 +18,26 @@ print $DEV chr(150);
 $chan1 = ord(getc($DEV));
 $time1 = time;
 
- print $DEV $cmd;
- print $DEV chr(151);
- $chan2 = ord(getc($DEV)); 
- $time2 = time;
-
-print "$chan1\t$chan2";
+if ($chan1 > 500) { # Puck 1 has detected a vehicle
+ my $time = time;
+ while (($time - time) <= $timeout) { # Start polling puck2 for $timeout
+  print $DEV $cmd;
+  print $DEV chr(151);
+  $chan2 = ord(getc($DEV)); 
+  $time2 = time;
+  if ($chan2 > 50) { # Puck 2 is alarmed
+   &CalculateSpeed();
+  }
+ }
+}
+&CalculateSpeed();
+sub CalculateSpeed{
+ my $time11 = time;
+ my $time22 = time + .1;
+ my $time = $time22 - $time11;
+ my $fps = $distance / $time;
+ my $mph = (($fps * 60) * 60) / 5280;
+ print "$mph\n";
+}
 
 close($DEV);
