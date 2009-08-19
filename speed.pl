e37cc5af6fa5e46e870952e2ecf49f691246575f
Kyle Johnson <kjohnson@fixertec.net>
2009-08-19 17:01:27 -0400

    Minor code changes

--- speed.pl
+++ speed.pl
@@ -13,31 +13,37 @@ our $timeout = 4;
 open(my $DEV, "+<", $port) || die "can't open $port: $!";
 
 # Get the status of puck 1
-print $DEV $cmd;
-print $DEV chr(150);
-$chan1 = ord(getc($DEV));
-$time1 = time;
+&Poll1();
 
-if ($chan1 > 500) { # Puck 1 has detected a vehicle
+if ($chan1 < 50) { # Puck 1 has detected a vehicle
  my $time = time;
  while (($time - time) <= $timeout) { # Start polling puck2 for $timeout
   print $DEV $cmd;
   print $DEV chr(151);
   $chan2 = ord(getc($DEV)); 
   $time2 = time;
-  if ($chan2 > 50) { # Puck 2 is alarmed
+  if ($chan2 < 50) { # Puck 2 is alarmed
    &CalculateSpeed();
+  } else {
+   select(undef,undef,undef,$polltime);
   }
  }
+} else {
+ &Poll1();
 }
-&CalculateSpeed();
+
 sub CalculateSpeed{
- my $time11 = time;
- my $time22 = time + .1;
- my $time = $time22 - $time11;
+ my $time = $time2 - $time1;
  my $fps = $distance / $time;
  my $mph = (($fps * 60) * 60) / 5280;
  print "$mph\n";
+}
+
+sub Poll1{
+ print $DEV $cmd;
+ print $DEV chr(150);
+ $chan1 = ord(getc($DEV));
+ $time1 = time;
 }
 
 close($DEV);
