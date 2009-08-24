ac0c4659db9eb3518c828ba60b69053cc3fe40d2
Kyle Johnson <kjohnson@fixertec.net>
2009-08-24 10:53:11 -0400

    Cleaned up code in speed.pl

--- speed.pl
+++ speed.pl
@@ -4,46 +4,48 @@ use strict;
 use warnings;
 
 my $port = "/dev/ttyUSB0";
-our $distance = 12; # Distance between pucks
+our $distance = 12; # Distance in feet between pucks
 our $polltime = .03125; # Time between each poll
 our $cmd = chr(254);
-our ($chan1, $chan2, $time1, $time2);
+our ($chan0, $chan1, $time0, $time1);
 our $timeout = 4;
 
 open(my $DEV, "+<", $port) || die "can't open $port: $!";
 
-# Get the status of puck 1
-&Poll1();
+&Poll(0);
 
-if ($chan1 < 50) { # Puck 1 has detected a vehicle
+if ($chan0 < 50) { # Puck 1 has detected a vehicle
  my $time = time;
  while (($time - time) <= $timeout) { # Start polling puck2 for $timeout
-  print $DEV $cmd;
-  print $DEV chr(151);
-  $chan2 = ord(getc($DEV)); 
-  $time2 = time;
-  if ($chan2 < 50) { # Puck 2 is alarmed
+  &Poll(1);
+  if ($chan1 < 50) { # Puck 2 is alarmed
    &CalculateSpeed();
   } else {
    select(undef,undef,undef,$polltime);
   }
  }
 } else {
- &Poll1();
+ &Poll();
 }
 
 sub CalculateSpeed{
- my $time = $time2 - $time1;
+ my $time = $time1 - $time0;
  my $fps = $distance / $time;
  my $mph = (($fps * 60) * 60) / 5280;
  print "$mph\n";
 }
 
-sub Poll1{
+sub Poll {
+ my $channel = 150 + $_[0];
  print $DEV $cmd;
- print $DEV chr(150);
- $chan1 = ord(getc($DEV));
- $time1 = time;
+ print $DEV $channel;
+ if ($channel == 150) {
+  $chan0 = ord(getc($DEV));
+  $time0 = time;
+ } elsif ($channel == 151) {
+  $chan1 = ord(getc($DEV));
+  $time1 = time;
+ }
 }
 
 close($DEV);
