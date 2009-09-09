701805dc5c0171ca0e384a7752f72161f9b4d253
Kyle Johnson <kjohnson@fixertec.net>
2009-09-09 12:05:28 -0400

    blah

--- 2speed.pl
+++ 2speed.pl
@@ -17,8 +17,8 @@ use strict;
 use warnings;
 use Time::HiRes qw(time);
 
-my $dev = "/dev/ttyUSB0";
-my $distance = 20; # Distance in feet between pucks
+my $dev = "/dev/ttyS0";
+my $distance = 8.3; # Distance in feet between pucks
 my $polltime = .032;
 my ($time1, $time2);
 my $limit = 45;
@@ -29,13 +29,13 @@ my $voltage = 255;
 system("/bin/stty $baud ignbrk -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -F $dev") == 0 || die($!);
 open(my $DEV, "+<", $dev) || die($!);
 
-while ($voltage == 255) {
+while ($voltage > $limit) {
  &Poll(0);
 }
 $time1 = time;
 $voltage = 255;
 
-while ($voltage == 255) {
+while ($voltage > $limit) {
  &Poll(1);
 }
 $time2 = time;
@@ -49,6 +49,7 @@ sub Poll {
  print $DEV chr(254);
  print $DEV chr($cmd);
  $voltage = ord(getc($DEV));
+ #print "$cmd\t $voltage\n";
 }
 
 sub CalculateSpeed {
