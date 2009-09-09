c516bfbc0e0399f6378d6f4d2d23e648240ea41b
Kyle Johnson <kjohnson@fixertec.net>
2009-09-09 17:08:11 -0400

    Cleaned up code and removed some comments

--- 2speed.pl
+++ 2speed.pl
@@ -1,18 +1,5 @@
 #!/usr/bin/perl
 
-#
-# This script is used to poll an ADSSR4xPROXR device, which is connected to
-# external sensys equipment, which is used to detect the presense of vehicles.
-# The "pucks" are the sensys equipement which is in the pavement.  Pucks
-# relay to an access point, which connects to our PROXR board.  Each puck
-# has it's own channel (P1 -> C1; P2 -> C2).  When a puck is not detecting
-# a vehicle, it outputs ~3.8v.  When a vehicle is detected, the puck outputs
-# <~.5v.  So when P1 voltage drops below $limit (how PROXR reports voltages,
-# before the Analog to Digital (A2D) conversion), record $time1, start polling
-# P2.  When P2 drops below $limit, record $time2, and then calculate the speed
-# of the vehicle.
-#
-
 use strict;
 use warnings;
 use Time::HiRes qw(time);
@@ -35,7 +22,6 @@ while ($voltage > $limit) {
 $time1 = time;
 $voltage = 255;
 
-
 while ($voltage > $limit) {
  &Poll(1);
 }
@@ -49,14 +35,13 @@ sub Poll {
  my $cmd = 150 + $_[0];
  print $DEV chr(254);
  print $DEV chr($cmd);
- my $time = time;
  $voltage = ord(getc($DEV));
 }
 
 sub CalculateSpeed {
  my $time = $time2 - $time1;
  my $fps = $distance / $time;
- my $mph = (($fps * 60) * 60) / 5280; # Or just $fps * .682?
+ my $mph = (($fps * 60) * 60) / 5280;
  print "$mph mph\n";
 }
 
