6992233cec7f000d0e570de3a5b25f029df2302c
Kyle Johnson <kjohnson@fixertec.net>
2009-09-10 11:09:28 -0400

    This script is simply used to get the temperature, when wired into A2D chan 8, of a PROXR device.

--- temp.pl
+++ temp.pl
@@ -7,14 +7,11 @@ my $port = "/dev/ttyUSB0";
 my $cmd = chr(254);
 my $chan = chr(157);
 
-open(my $DEV, "+<", $port) || die "can't open $port: $!\n";
-
-print $DEV $cmd;
-print $DEV $chan;
-
-my $Tc = ord(getc($DEV));
-my $Tf = (9/5)*$Tc+32;
-
-print $Tc . "C \t " . $Tf . "F\n";
-
+open(my $DEV, "+<", $port) || die($!);
+ print $DEV $cmd;
+ print $DEV $chan;
+ my $Tc = ord(getc($DEV));
 close($DEV);
+
+my $Tf = (9/5)*$Tc+32;
+print $Tc . "C \t " . $Tf . "F\n";
