8a3663dff3a1d5376211df1e385e271704c7f70e
Kyle Johnson <kjohnson@fixertec.net>
2009-09-10 17:01:57 -0400

    Modified code to make better use of the POE Framework.  Still lots to do.

--- traffiscript.pl
+++ traffiscript.pl
@@ -9,12 +9,14 @@ use Time::HiRes;
 my $bank = 1; # Bank number which relays on device belong to
 my $baud = 115200;
 my $color = 'green'; # Set initial color to Green
+my $detect :shared = 'off';
 my $distance = 8.3; # Distance in feet between pucks
-my $filename = "/var/www/zm/events/2/dbgpipe.log"; # The file for this script to monitor
+my $filename = "/var/www/zm/events/stop-light/dbgpipe.log"; # The file for this script to monitor
 my $limit = 45; # Voltage limit while polling for traffic
 my $logfile = "/var/log/lightrelay.log"; # Where to output color changes to
 my $polltime = .032;
 my $port = "/dev/ttyS0";
+my $sleeptime = .1;
 my ($time1, $time2);
 my $voltage = 255;
 my $on_green = 108;
@@ -31,69 +33,42 @@ open(my $PORT, "+<", "$port") || die($!);
 
 POE::Session->create(
  inline_states => {
-  _start => sub {
-   $_[HEAP]{tailor} = POE::Wheel::FollowTail->new(
-    Filename => "$filename",
-    InputEvent => "got_log_line",
-    ResetEvent => "got_log_rollover",
-   );
-  },
-  got_log_line => sub {
-   if ($_[ARG0] =~ /Green.*alarmed/ && $color eq 'red') # Color is red; green alarms...
-    {
-     &do_stuff($off_red,$on_green,'green','Green');
-    }
-   elsif ($_[ARG0] =~ /LG.*alarmed/ && $color eq 'red') # Color is red; left green alarms...
-    {
-     &do_stuff($off_red,$on_green,'green','Left Green');
-    }
-   elsif ($_[ARG0] =~ /Amber.*alarmed/ && $color eq 'green') # Color is green; amber alarms...
-    {
-     &do_stuff($off_green,$on_amber,'amber','Amber');
-    }
-   elsif ($_[ARG0] =~ /Red.*alarmed/ && $color eq 'amber') # Color is amber; red alarms...
-    {
-     &do_stuff($off_amber,$on_red,'red','Red');
-    }
-  },
-  got_log_rollover => sub {
-   print "Log rolled over.\n";
-  },
+  _start => \&server_start,
+  got_log_line => \&got_log_line,
  }
 );
- POE::Kernel->run();
+POE::Kernel->run();
 
-
+sub server_start {
+ $_[HEAP]{tailor} = POE::Wheel::FollowTail->new(
+  Filename => "$filename",
+  InputEvent => "got_log_line",
+ );
+ print "Server started\n";
 
 sub do_stuff {
- $color = $_[2]; # Set color
+$color = $_[2]; # Set color
 
- &switch_relay($_[0],$_[1]);
- &DetectTraffic();
- &log($_[3]);
-
+switch_relay($_[0],$_[1]);
+DetectTraffic();
+#log($_[3]);
 }
-
-sub CalculateSpeed {
- my $time = $time2 - $time1;
- my $fps = $distance / $time;
- my $mph = (($fps * 60) * 60) / 5280;
- 
 }
 
 sub DetectTraffic {
- while ($voltage > $limit) {
-  &Poll(0);
- }
- $time1 = time;
- $voltage = 255;
+  while ($voltage > $limit) {
+   Poll(0);
+  }
+  $time1 = time;
+  $voltage = 255;
 
- while ($voltage > $limit) {
-  &Poll(1);
- }
- $time2 = time;
+  while ($voltage > $limit) {
+   Poll(1);
+  }
+  $time2 = time;
 
- &CalculateSpeed();
+  trigger_zm();
+  CalculateSpeed();
 }
 
 sub Poll {
@@ -102,11 +77,6 @@ sub Poll {
  print $PORT chr(254);
  print $PORT chr($cmd);
  $voltage = ord(getc($PORT));
-}
-
-
-sub trigger_zm {
-
 }
 
 sub switch_relay {
@@ -118,6 +88,40 @@ sub switch_relay {
  print $PORT chr(254); # Enter Command Mode
  print $PORT chr($_[1]); # Activate Current Relay
  print $PORT chr($bank); # In Bank 1
+}
+
+ 
+sub got_log_line {
+ print "$_[ARG0]\n";
+
+ if ($_[ARG0] =~ /Green.*alarmed/ && $color eq 'red') # Color is red; green alarms...
+ {
+  do_stuff($off_red,$on_green,'green','Green');
+ }
+ elsif ($_[ARG0] =~ /LG.*alarmed/ && $color eq 'red') # Color is red; left green alarms...
+ {
+  do_stuff($off_red,$on_green,'green','Left Green');
+ }
+ elsif ($_[ARG0] =~ /Amber.*alarmed/ && $color eq 'green') # Color is green; amber alarms...
+ {
+  do_stuff($off_green,$on_amber,'amber','Amber');
+ }
+ elsif ($_[ARG0] =~ /Red.*alarmed/ && $color eq 'amber') # Color is amber; red alarms...
+ {
+  do_stuff($off_amber,$on_red,'red','Red');
+ }
+}
+
+
+sub CalculateSpeed {
+ my $time = $time2 - $time1;
+ my $fps = $distance / $time;
+ my $mph = (($fps * 60) * 60) / 5280;
+}
+
+
+sub trigger_zm {
+
 }
 
 sub log {
