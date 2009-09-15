21ee3f894981eb48c01fe95ab1aabc1db3580c36
Kyle Johnson <kjohnson@williston-linuxpc.cheers.local>
2009-09-15 13:57:43 -0400

    Added more verbosity

--- poespeed.pl
+++ poespeed.pl
@@ -12,6 +12,7 @@ my ($time1, $time2);
 my $limit = 45;
 my $baud = 115200;
 my $timeout = 2; # Poll puck 2 for only this long
+#my $voltage = 255;
 
 system("/bin/stty $baud ignbrk -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -F $dev") == 0 || die($!);
 open(my $DEV, "+<", $dev) || die($!);
@@ -30,23 +31,25 @@ exit;
 
 sub detect_traffic {
  my ($kernel, $heap) = @_[KERNEL, HEAP];
- $heap->{voltage} = 255;
 
  if ($heap->{voltage} > $limit) {
+# if ($voltage > $limit) {
   $kernel->yield("poll", 0);
   $kernel->yield("detect_traffic");
   return;
  }
  $time1 = time;
  $heap->{voltage} = 255;
+ #$voltage = 255;
 
  if ($heap->{voltage} > $limit) {
+# if ($voltage > $limit) {
   $kernel->yield("poll", 1);
   $kernel->yield("detect_traffic");
   return;
  }
  $time2 = time;
-
+ print "Am I getting here?\n";
  $kernel->yield("calculate_speed");
 }
 
@@ -54,20 +57,23 @@ sub poll {
  my ($kernel, $heap, $arg) = @_[KERNEL, HEAP, ARG0];
  select(undef,undef,undef,$polltime);
  my $cmd = 150 + $arg;
- print 
  print $DEV chr(254);
  print $DEV chr($cmd);
  $heap->{voltage} = ord(getc($DEV));
+ #$voltage = ord(getc($DEV));
+ print "$cmd is $heap->{voltage}\n";
 }
 
 sub calculate_speed {
  my ($kernel, $heap) = @_[KERNEL, HEAP];
+ print "Calculating speed...\n";
  my $time = $time2 - $time1;
  my $fps = $distance / $time;
  my $mph = (($fps * 60) * 60) / 5280;
  $mph = sprintf("%.2f", $mph);
  print "$mph mph\n\n";
  $heap->{voltage} = 255;
+ #$voltage = 255;
  sleep(1);
  $kernel->yield("detect_traffic");
 }
