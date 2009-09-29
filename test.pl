013c0616450120633925610e104320a403e5e16c
Kyle Johnson <kjohnson@fixertec.net>
2009-09-29 14:14:36 -0400

    Initial commit of test.pl
     * Used to turn on and off all relays, or individual relays

---
+++ test.pl
@@ -0,0 +1,41 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $cmd = shift;
+
+if (!$cmd) {
+ die("You must enter a command!\n");
+ exit;
+}
+
+if ($cmd eq "on") {
+ print "Turning relays on...\n";
+ $cmd = 130;
+}
+if ($cmd eq "off") {
+ print "Turning relays off...\n";
+ $cmd = 129;
+}
+if ($cmd eq "green") {
+ print "Turning $cmd on...\n";
+ $cmd = 108;
+}
+if ($cmd eq "amber") {
+ print "Turning $cmd on...\n";
+ $cmd = 109;
+}
+if ($cmd eq "red") {
+ print "Turning $cmd on...\n";
+ $cmd = 110;
+}
+
+open(my $DEV, "+<", "/dev/ttyS0") || die ($!);
+ print $DEV chr(254);
+ print $DEV chr($cmd);
+ print $DEV chr(1);
+
+ my $result = ord(getc($DEV));
+ print "$result\n";
+close($DEV);
