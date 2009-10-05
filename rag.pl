c07b88e70008e8f550755b105cbac3b53fdf4aad
Kyle Johnson <kjohnson@williston-linuxpc.cheers.local>
2009-10-05 14:55:53 -0400

    Changed sleep times

--- rag.pl
+++ rag.pl
@@ -3,14 +3,15 @@
 use strict;
 use warnings;
 
+
 open(my $FILE, ">>", "dbgpipe.log") || die($!);
 select((select($FILE), $| = 1)[0]);
 while () {
  print $FILE "Amber alarmed\n";
  sleep(3);
  print $FILE "Red alarmed\n";
- sleep(3);
+ sleep(10);
  print $FILE "Green alarmed\n";
- sleep(3);
+ sleep(20);
 }
 close($FILE);
