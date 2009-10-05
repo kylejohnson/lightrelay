ad64426b46b6a1ee1fb148240d0b921aaba0ec63
Kyle Johnson <kjohnson@fixertec.net>
2009-10-05 14:47:51 -0400

    Simple script to write red, amber and green color changes to file, mimicing the output of ZoneMinder.

---
+++ rag.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+open($FILE, ">>", "dbgpipe.log") || die($!);
+select((select($FILE), $| = 1)[0]);
+while () {
+ print $FILE "Amber alarmed\n";
+ sleep(3);
+ print $FILE "Red alarmed\n";
+ sleep(3);
+ print $FILE "Green alarmed\n";
+ sleep(3);
+}
+close($FILE);
