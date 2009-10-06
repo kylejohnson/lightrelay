4b492e1caf3ee8312303641205ea7b537e115bfb
Kyle Johnson <kjohnson@williston-linuxpc.cheers.local>
2009-10-06 13:41:58 -0400

    Changed sleep times back to 3

--- rag.pl
+++ rag.pl
@@ -10,8 +10,8 @@ while () {
  print $FILE "Amber alarmed\n";
  sleep(3);
  print $FILE "Red alarmed\n";
- sleep(10);
+ sleep(3);
  print $FILE "Green alarmed\n";
- sleep(20);
+ sleep(3);
 }
 close($FILE);
