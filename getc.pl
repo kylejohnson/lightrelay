e493be0e954b79483ae5ed727bd5b8a5fe98d5d5
Kyle Johnson <kjohnson@fixertec.net>
2009-10-13 14:13:27 -0400

    Minor code changes
     * Lexical File Handles
     * Changed path to device

--- getc.pl
+++ getc.pl
@@ -3,13 +3,12 @@
 use strict;
 use warnings;
 
-my $port = "/dev/ttyUSB0";
+my $port = "/dev/ttyS0";
 my $cmd = shift;
 
-open (SERIALPORT, "+<", "$port") || ($!);
-print SERIALPORT chr(254);
-print SERIALPORT chr($cmd);
-my $result = getc(SERIALPORT);
-print ord($result) . "\n";
-
-close (SERIALPORT);
+open (my $SERIALPORT, "+<", "$port") || ($!);
+ print $SERIALPORT chr(254);
+ print $SERIALPORT chr($cmd);
+ my $result = getc($SERIALPORT);
+ print ord($result) . "\n";
+close ($SERIALPORT);
