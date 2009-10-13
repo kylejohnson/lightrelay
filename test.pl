9c3f48ff694049c6eb152bdab186d1e9e301ba01
Kyle Johnson <kjohnson@fixertec.net>
2009-10-13 17:02:32 -0400

    Added code to print result of each relay test

--- test.pl
+++ test.pl
@@ -16,7 +16,7 @@ if (!$cmd) {
 }
 
 if ($cmd eq "test") {
- print "Testing 2-way communication with $dev...\n";
+ print "Testing 2-way communication with $dev!\n";
  # We want to first set the baud to 38400, send a command, and if we receive a response,
  # let us know, then continue testing each relay.
  # If not, move on to 115200 and do the same thing.
@@ -63,15 +63,19 @@ if ($cmd eq "off") {
 
 
 sub test_relays {
- print "Communications with $dev succedded at $baud.  Testing indivudual relays...\n";
- my $answer = send_cmds(108, 109, 110, 100, 101, 102);
+ print "Communications succedded at $baud!  Testing indivudual relays...\n";
+ my @answers = send_cmds(108, 109, 110, 100, 101, 102);
+ foreach (@answers) {
+  print "$_\n";
+ }
 }
 
 sub send_cmds {
  my $answer;
+ my @answers;
  foreach my $cmd (@_) {
   sleep(1);
-  local $SIG{ALRM} = sub{die "Timed out communicating with $dev at $baud...\n"};
+  local $SIG{ALRM} = sub{die "Timed out while trying to communicate at $baud...\n"};
   eval {
    alarm $timeout;
    open(my $DEV, "+<", $dev) || die ($!);
@@ -83,12 +87,15 @@ sub send_cmds {
      print $DEV chr($bank);
     }
     $answer = ord(getc($DEV));
+    push @answers, $answer;
    close($DEV);
    alarm 0;
   }
  }
- if ($answer) {
+ if ($cmd = 27 && $answer) {
   return $answer;
+ } elsif (@answers) {
+  return @answers;
  } else {
   return 0;
  }
@@ -96,28 +103,6 @@ sub send_cmds {
 
 sub stty {
  my $baud = shift;
- print "Setting baud rate of $dev to $baud...\n";
+ print "Setting baud rate to $baud...\n";
  system("/bin/stty $baud ignbrk -brkint -icrnl -imaxbel -opost -isig -icanon -iexten -echo -F $dev") == 0 || die($!);
-}
-
-
-if ($cmd eq "on") {
- print "Turning relays on...\n";
- $cmd = 130;
-}
-if ($cmd eq "off") {
- print "Turning relays off...\n";
- $cmd = 129;
-}
-if ($cmd eq "green") {
- print "Turning $cmd on...\n";
- $cmd = 108;
-}
-if ($cmd eq "amber") {
- print "Turning $cmd on...\n";
- $cmd = 109;
-}
-if ($cmd eq "red") {
- print "Turning $cmd on...\n";
- $cmd = 110;
 }
